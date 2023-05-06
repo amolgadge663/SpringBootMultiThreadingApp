c77a3b793a950f6a45cfcaf84cd7cf1df32fc170
Amol Kailas Gadage <amol.gadage@incedoinc.com>
2023-05-06 13:15:55 +0530

    initial commit

---
+++ src/main/java/com/amolsoftwares/www/mymultithreadingapp/MyMultiThreadingAppApplication.java
@@ -0,0 +1,13 @@
+package com.amolsoftwares.www.mymultithreadingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMultiThreadingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMultiThreadingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amolsoftwares/www/mymultithreadingapp/MyMultiThreadingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amolsoftwares.www.mymultithreadingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMultiThreadingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
